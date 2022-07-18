21013e9227757c02da77f68d7011b19534148a05
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-07-17 22:55:34 -0300

    ok

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9e6a4c39cf6e918d179bd4e2604d57cd97266420',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9e6a4c39cf6e918d179bd4e2604d57cd97266420',
+            'dev_requirement' => false,
+        ),
+    ),
+);
