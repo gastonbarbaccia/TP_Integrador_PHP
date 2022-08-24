abf1eae5a7a49cf2eb4165173af5edf55f6c11c5
gastonbarbaccia <40251128+gastonbarbaccia@users.noreply.github.com>
2022-08-24 14:15:19 -0300

    Update nuevo.php

--- nuevo.php
+++ nuevo.php
@@ -136,7 +136,7 @@
 						 <div class="form-group row">
              <label for="staticEmail" class="col-sm-ggb col-form-label">Imagen</label>
 							<div class="col-sm-3">
-								<input type="file" class="form-control-ggb form-control" id="archivo" name="archivo">
+								<input type="file" class="form-control-ggb form-control" id="archivo" name="archivo" accept="image/jpeg">
 							</div>
 						</div>
              <div class="card-tools">
